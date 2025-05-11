77d827cd153d57f1beab95f9bb170714a35e9c7e
sanjukumari-tech <foodlazzez@gmail.com>
2025-05-11 22:53:42 +0530

    socket

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://kazam-note-backendk.onrender.com',
         changeOrigin: true,
 
       }
